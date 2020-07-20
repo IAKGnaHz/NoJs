e78d311890447353fdb4cf71b10af3fd7ab308bb
zhangkai <kaizhang@91jkys.com>
2020-07-20 11:13:04 +0800

    feat:新增全局过滤器

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { logger } from './middleware/function.middleware'
+import { AllExceptionsFilter } from './error/all-exceptions.filter'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // 新增全局中间件
+  const { httpAdapter } = app.get(HttpAdapterHost);
   app.use(logger);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
   await app.listen(3000);
 }
 bootstrap();
